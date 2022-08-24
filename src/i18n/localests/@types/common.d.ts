99dcf854f0c9f0164a2d4e2da4e3c044dfc2f287
Alice Chou <alice1qaz@gmail.com>
2022-08-24 18:01:44 +0800

    style(home): modify TradingRules and it's i18n

--- src/i18n/localests/@types/common.d.ts
+++ src/i18n/localests/@types/common.d.ts
@@ -38,12 +38,116 @@ export type Common = {
   transparencyOfTheTradingEnvironment: string;
   /** Give you a trusted platform */
   giveYouATrustedPlatform: string;
+
   /** TRADING RULES starts */
   /** Trading */
   trading: string;
   /** Rules */
   rules: string;
+  /** Product code */
+  productCode: string;
+  /** gold */
+  gold: string;
+  /** silver */
+  silver: string;
+  /** Account currency */
+  accountCurrency: string;
+  /** dollar */
+  dollar: string;
+  /** Trading platform */
+  tradingPlatform: string;
+  /** Margin requirement */
+  marginRequirement: string;
+  /** Transaction fee */
+  transactionFee: string;
+  /** Zero transaction fee */
+  zeroTransactionFee: string;
+  /** Contract lot size */
+  ContractLotSize: string;
+  /** ounce */
+  ounce: string;
+  /** Minimum  */
+  minimum: string;
+  /** Maximum  */
+  maximum: string;
+  /** lot */
+  lot: string;
+  /** bid-ask spread */
+  bidAskSpread: string;
+  /** Premium */
+  premium: string;
+  /** Superior */
+  superior: string;
+  /** Basic  */
+  basic: string;
+  /** fixed  */
+  fixed: string;
+  /** lock-up margin */
+  lockUpMargin: string;
+  /** of the total amount of each contract */
+  ofTheTotalAmountOfEachContract: string;
+  /** liquidation level */
+  liquidationLevel: string;
+  /** If the margin level */
+  ifTheMarginLevel: string;
+  /** MetaTrader 4 will automatically */
+  MetaTrader4WillAutomatically: string;
+  /** When the price fluctuates greatly */
+  whenThePriceFluctuatesGreatly: string;
+  /** Weekend/Holiday Market */
+  weekendHolidayMarket: string;
+  /** If the margin is less */
+  ifTheMarginIsLess: string;
+  /** market interest */
+  marketInterest: string;
+  /** One-day market passing interest */
+  OneDayMarketPassingInterest: string;
+  /** Interest will be adjusted */
+  interestWillBeAdjusted: string;
+  /** Instruction category */
+  instructionCategory: string;
+  /** Market price */
+  marketPrice: string;
+  /** Order time limit */
+  orderTimeLimit: string;
+  /** Valid until the close  */
+  validUntilTheClose: string;
+  /** Limit/Stop Loss and Take Profit */
+  limitStopLossAndTakeProfit: string;
+  /** It needs to be more than */
+  itNeedsToBeMoreThan: string;
+  /** Before major data is released */
+  beforeMajorDataIsReleased: string;
+  /** Deposit and withdrawal */
+  depositAndWithdrawal: string;
+  /** USD to HKD */
+  USDToHKD: string;
+  /** Trading Hours */
+  tradingHours: string;
+  /** Trading Hours summer */
+  tradingHoursSummer: string;
+  /** Trading Hours winter */
+  tradingHoursWinter: string;
+  /** System Settlement */
+  systemSettlement: string;
+  /** System Settlement summer */
+  systemSettlementSummer: string;
+  /** System Settlement winter */
+  systemSettlementWinter: string;
+  /** System Maintenance */
+  systemMaintenance: string;
+  /** System Maintenance summer */
+  systemMaintenanceSummer: string;
+  /** System Maintenance winter */
+  systemMaintenanceWinter: string;
+  /** Premium account */
+  premiumAccount: string;
+  /** Superior account */
+  superiorAccount: string;
+  /** Since the price fluctuations will be extremely violent  */
+  sinceThePriceFluctuations: string;
   /** TRADING RULES ends */
+
   /** software */
   software: string;
   /** download */
