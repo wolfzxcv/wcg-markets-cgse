9f3a7afa198b836ed8262f891d9fbfc73209cef5
Alice Chou <alice1qaz@gmail.com>
2022-09-08 15:55:30 +0800

    style(links.ts): modify tel in links.ts

--- src/assets/links.ts
+++ src/assets/links.ts
@@ -23,7 +23,7 @@ export const links = {
     process.env.NEXT_PUBLIC_USERS_CENTER ||
     'https://my.wcgmarkets-global.com/login',
   // 聯絡電話
-  tel: '4001 203 612',
+  tel: '+85238963608',
   // 聯絡信箱
   email: 'support@wcghk.com'
 };
