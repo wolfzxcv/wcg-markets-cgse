1f9f6e32d28c0d13088c33ba969597b777f9541d
Alice Chou <alice1qaz@gmail.com>
2022-09-28 11:20:02 +0800

    style(awardwinningplatform): modify styling of AwardWinningPlatform

--- src/components/About/AwardWinningPlatform.tsx
+++ src/components/About/AwardWinningPlatform.tsx
@@ -1,9 +1,13 @@
-import { COMMON_FONT_COLOR, FONT_WEIGHT } from '@/assets/styleConfig';
+import {
+  COMMON_FONT_COLOR,
+  FONT_WEIGHT,
+  SMART_CHOICE_BG
+} from '@/assets/styleConfig';
 import { Locales } from '@/i18n/config';
 import { Box, Flex, Image, Stack } from '@chakra-ui/react';
 import { useTranslation } from 'next-i18next';
 import { useRouter } from 'next/router';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const AwardWinningPlatform: React.FC<{}> = () => {
   const { t } = useTranslation('about');
@@ -16,18 +20,18 @@ const AwardWinningPlatform: React.FC<{}> = () => {
 
   const element = useRef();
 
-  const heightOfEachElement = 800;
+  const heightOfEachElement = 750;
 
-  // the position of y-axis of element
-  // @ts-ignore
-  const elementY = element.current?.offsetTop;
-
-  const determinePic = (currentY: number) => {
-    const breakPointStart = elementY + heightOfEachElement * 0.3;
-    const breakPoint1 = elementY + heightOfEachElement * 1.0;
-    const breakPoint2 = elementY + heightOfEachElement * 1.8;
-    const breakPoint3 = elementY + heightOfEachElement * 2.6;
-    const breakPoint4 = elementY + heightOfEachElement * 3.5;
+  const determinePic = () => {
+    // the position of y-axis of element
+    // @ts-ignore
+    const elementY = element.current?.offsetTop;
+    const currentY = window.scrollY;
+    const breakPointStart = elementY;
+    const breakPoint1 = elementY + heightOfEachElement * 0.8;
+    const breakPoint2 = elementY + heightOfEachElement * 1.6;
+    const breakPoint3 = elementY + heightOfEachElement * 2.4;
+    const breakPoint4 = elementY + heightOfEachElement * 3.2;
 
     if (currentY > breakPointStart && currentY < breakPoint1) {
       setPic(defaultPic);
@@ -46,48 +50,55 @@ const AwardWinningPlatform: React.FC<{}> = () => {
     }
   };
 
+  useEffect(() => {
+    window.addEventListener('scroll', determinePic);
+
+    return () => {
+      window.removeEventListener('scroll', determinePic);
+    };
+  }, []);
+
   return (
-    <Box
-      ref={element}
-      onWheel={(e: React.MouseEvent) => determinePic(e.pageY)}
-      onTouchEnd={(e: React.TouchEvent) =>
-        determinePic(e.changedTouches[0].pageY)
-      }
-      onMouseUp={(e: React.MouseEvent) => determinePic(e.pageY)}
-      onMouseMove={(e: React.MouseEvent) => determinePic(e.pageY)}
-      onMouseLeave={() => setShow(false)}
-    >
-      <AwardWinningPlatformEach
-        title={t('awardTitle1')}
-        text={t('awardText1')}
-      />
+    <>
+      <Box ref={element}>
+        <AwardWinningPlatformEach
+          title={t('awardTitle1')}
+          text={t('awardText1')}
+        />
 
-      <AwardWinningPlatformEach
-        title={t('awardTitle2')}
-        text={t('awardText2')}
-      />
+        <AwardWinningPlatformEach
+          title={t('awardTitle2')}
+          text={t('awardText2')}
+        />
 
-      <AwardWinningPlatformEach
-        title={t('awardTitle3')}
-        text={t('awardText3')}
-      />
+        <AwardWinningPlatformEach
+          title={t('awardTitle3')}
+          text={t('awardText3')}
+        />
 
-      <AwardWinningPlatformEach
-        title={t('awardTitle4')}
-        text={t('awardText4')}
-      />
+        <AwardWinningPlatformEach
+          title={t('awardTitle4')}
+          text={t('awardText4')}
+        />
 
-      <Image
-        width={{ base: '80vw', md: '50%' }}
-        maxWidth="750px"
-        display={show ? 'block' : 'none'}
-        position="fixed"
-        top="150px"
-        right={{ base: '50px', md: '10%' }}
-        src={`/../assets/images/award_winning_platform_${pic}.png`}
-        alt="award_winning_platform"
-      />
-    </Box>
+        <Image
+          width={{ base: '80vw', md: '50%' }}
+          maxWidth="750px"
+          display={show ? 'block' : 'none'}
+          position="fixed"
+          top={{ base: 'unset', md: '150px' }}
+          bottom={{ base: '150px', md: 'unset' }}
+          right={{ base: '50px', md: '10%' }}
+          src={`/../assets/images/award_winning_platform_${pic}.png`}
+          alt="award_winning_platform"
+        />
+      </Box>
+      <Box
+        display={{ base: 'block', md: 'none' }}
+        bg={SMART_CHOICE_BG}
+        height="600px"
+      ></Box>
+    </>
   );
 };
 
